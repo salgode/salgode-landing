e136ab3baf0df317dfe51b9bf875ef2ef7ee2f57
Franco Méndez <fnmendez@uc.cl>
2019-11-06 23:43:33 -0300

    chore: update general app information

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# landing-page-salgode
-landing page app SalgoDe 
+# Salgode Landing
 
+Landing page de la aplicación SalgoDe, una aplicación gratuita para compartir viajes.
 
-Crear .env con REACT_APP_EMAILJS_TOKEN=<<token>> // Token de https://www.emailjs.com/
+La app móvil se encuentra [aquí](https://github.com/salgode/salgode-mobile).
+La app web se encuentra [aquí](https://github.com/salgode/salgode-web).
+El backend se encuentra [aquí](https://github.com/Varuscl/salgode-api).
+
+Sigue el desarrollo en [nuestro Trello](https://trello.com/b/GCTJ1iMU/salgode).
+
+## Desarrollo
+
+  - Las ramas `feat/*`, `fix/*`, `chore/*`, `hotfix/*` and `docs/*` se ven bien con `dash-case`.
+
+  - Usamos **squash and merge** a `dev` usando [conventional commits](https://conventionalcommits.org).
+
+  - Crear .env con REACT_APP_EMAILJS_TOKEN=<<token>> // Token de https://www.emailjs.com/
+
+## Agradecimientos
+
+Se aprecia el aporte por gestionar el equipo de desarrollo a:
+
+- [Matías Andrade](https://github.com/mandrade2)
+- [José Morales Lira](https://github.com/josemlira)
+- [Franco Méndez Z.](https://github.com/fnmendez)
--- src/routes/MainView.js
+++ src/routes/MainView.js
@@ -1,57 +1,57 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Hidden from "@material-ui/core/Hidden";
-import Box from "@material-ui/core/Box";
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Hidden from '@material-ui/core/Hidden';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    height: "100%"
+    height: '100%',
   },
   img: {
-    width: "50%",
-    height: "58%"
+    width: '50%',
+    height: '58%',
   },
   content: {
-    textAlign: "center"
+    textAlign: 'center',
   },
   overlay: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     bottom: 0,
     right: 0,
     left: 0,
-    backgroundColor: "rgba(0,0,0,.3)"
+    backgroundColor: 'rgba(0,0,0,.3)',
   },
   mainFeaturedPost: {
-    position: "relative",
+    position: 'relative',
     color: theme.palette.common.white,
     marginBottom: theme.spacing(4),
-    backgroundColor: "blue",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center"
+    backgroundColor: 'blue',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
   },
   mainFeaturedPostContent: {
-    position: "relative",
+    position: 'relative',
     padding: theme.spacing(3),
-    [theme.breakpoints.up("md")]: {
+    [theme.breakpoints.up('md')]: {
       padding: theme.spacing(6),
-      paddingRight: 0
-    }
+      paddingRight: 0,
+    },
   },
   card: {
-    display: "flex"
+    display: 'flex',
   },
   cardDetails: {
-    flex: 1
+    flex: 1,
   },
   cardMedia: {
     width: 250,
@@ -61,8 +61,8 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: 'Kepler296',
   },
   title: {
-    fontFamily: "Kepler296"
-  }
+    fontFamily: 'Kepler296',
+  },
 }));
 
 export default function MainView() {
@@ -95,9 +95,9 @@ export default function MainView() {
         </Paper>
       </main>
 
-      {/*CARD */}
+      {/* CARD */}
       <Grid container spacing={4}>
-        <Grid item key={"SalgoDe"} xs={12} md={12}>
+        <Grid item key="SalgoDe" xs={12} md={12}>
           <CardActionArea component="a" href="#">
             <Card className={classes.card}>
               <div className={classes.cardDetails}>
@@ -115,7 +115,7 @@ export default function MainView() {
                   <Typography variant="subtitle1" paragraph>
                     Si tenemos el mismo fin, ¡compartamos el viaje!
                     <br />
-                    SALGO DE es una aplicación para traslados solidarios en
+                    SalgoDe es una aplicación para traslados solidarios en
                     momentos de emergencia nacional y crisis de transporte.
                     <br />
                     Con el fin de lograr que todos lleguen sanos y salvos a su
@@ -123,14 +123,16 @@ export default function MainView() {
                     automóvil y personas sin medio de transporte para que viajen
                     juntos.
                     <br />
-                    Todos podemos generar el cambio, <em>¡COMPARTE TU AUTO!</em>
+                    Todos podemos generar el cambio,
+                    {' '}
+                    <em>¡COMPARTE TU AUTO!</em>
                   </Typography>
                 </CardContent>
               </div>
               <Hidden xsDown>
                 <CardMedia
                   className={classes.cardMedia}
-                  image={require("../assets/car.png")}
+                  image={require('../assets/car.png')}
                   title="Image title"
                 />
               </Hidden>
