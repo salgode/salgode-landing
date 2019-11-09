d191a5d9472352fdcd614084357f285964b5da7a
Matias Andrade <mandrade2@uc.cl>
2019-11-09 13:50:56 -0300

    feat: terms and conditions

---
+++ src/routes/TermsView.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { minHeight } from "@material-ui/system";
+const TermsView = () => (
+  <div style={{ minHeight: "500px" }}>
+    <iframe
+      style={{ height: "100%", width: "100%" }}
+      src="https://docs.google.com/document/d/e/2PACX-1vSJmmCaNNemuu3LuTQxeI8FeY_GF7jGxem5ikAr_8gX2UEzddQ6ceddnZGz5nAoGw/pub?embedded=true"
+    ></iframe>
+  </div>
+);
+
+export default TermsView;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,12 @@
-
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import AppContainer from '../components/AppContainer/AppContainer';
-import MainView from './MainView';
-import HowToView from './HowToView';
-import DownloadView from './DownloadView';
-import ContactView from './ContactView';
-import NotFoundView from './NotFoundView';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AppContainer from "../components/AppContainer/AppContainer";
+import MainView from "./MainView";
+import HowToView from "./HowToView";
+import DownloadView from "./DownloadView";
+import ContactView from "./ContactView";
+import NotFoundView from "./NotFoundView";
+import TermsView from "./TermsView";
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -15,6 +15,7 @@ const AppRouter = () => (
         <Route path="/como-funciona" component={HowToView} />
         <Route path="/descarga" component={DownloadView} />
         <Route path="/contactanos" component={ContactView} />
+        <Route path="/terminos" component={TermsView} />
         <Route path="/" component={MainView} />
         <Route component={NotFoundView} />
       </Switch>
